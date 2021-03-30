e3a32a026c7e661113ff0eefa15450eef4c85592
MOREL Matthieu <matthieu.morel@cnp.fr>
2021-03-30 11:28:27 +0200

    components are handled by .babelrc

--- src/CodeSnippet.js
+++ src/CodeSnippet.js
@@ -3,21 +3,6 @@ import PropTypes from "prop-types"
 import hardyHAR from 'hardyhar'
 import Prism from "prismjs"
 
-/**
- * Importing manually since prism method loadlanguages is a dynamic import
- * which isn't supported by rollup.
- */
-import "prismjs/components/prism-bash.min"
-import "prismjs/components/prism-javascript.min"
-import "prismjs/components/prism-php.min"
-import "prismjs/components/prism-go.min"
-import "prismjs/components/prism-ruby.min"
-import "prismjs/components/prism-ocaml.min"
-import "prismjs/components/prism-ruby.min"
-import "prismjs/components/prism-java.min"
-import "prismjs/components/prism-python.min"
-import "prismjs/components/prism-swift.min"
-import "prismjs/components/prism-csharp.min"
 
 export default class CodeSnippet extends React.Component {
   constructor(props) {
